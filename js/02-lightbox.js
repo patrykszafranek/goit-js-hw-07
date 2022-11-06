5ed5a5c27fa7bb86291164ecafc7ad75237d0c47
Patryk Szafranek <patryk.szafranek94@gmail.com>
2022-11-06 14:32:48 +0100

    task 2 - done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const imageLink = document.createElement("a");
+  imageLink.setAttribute("href", item.original);
+  imageLink.classList.add("gallery__item");
+
+  const image = document.createElement("img");
+  image.src = item.preview;
+  image.setAttribute("alt", item.description);
+  image.setAttribute("data-source", item.original);
+  image.classList.add("gallery__image");
+
+  imageLink.append(image);
+  gallery.append(imageLink);
+});
+
+var lightbox = new SimpleLightbox(".gallery .gallery__item", {
+  /* options */
+  captions: true,
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  enableKeyboard: true,
+});
+
+lightbox.on("show.simplelightbox", function () {
+  console.log("lighbox otwarty");
+});
+
+lightbox.on("closed.simplelightbox", function () {
+  console.log("lightbox zamkniety");
+});
